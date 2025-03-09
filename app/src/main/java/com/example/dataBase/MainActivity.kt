7ffc8317340390ad310471e4aa1d8a96448ba7a1
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-03-09 21:19:47 +0000

    Creation de la classe DBconnexion

--- app/src/main/java/com/example/dataBase/MainActivity.kt
+++ app/src/main/java/com/example/dataBase/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.dataBase
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,40 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun Enregistrer(view: View) {
+        val nom: TextView = findViewById(R.id.chName)
+        val ls: ListView = findViewById(R.id.maliste)
+        val db = DBconnexion(this)
+        db.insertNewAdmin(nom.text.toString())
+        val arrayListe: ArrayList<String> = db.getAllRecord()
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayListe)
+        ls.adapter = myAdapter
+        nom.text = ""
+    }
+
+    fun Update(view: View) {
+        val chNom: TextView = findViewById(R.id.chName)
+        val chID: EditText = findViewById(R.id.chID)
+        val ls: ListView = findViewById(R.id.maliste)
+        val db = DBconnexion(this)
+        db.updateRow(chNom.text.toString(), chID.text.toString().toInt())
+        val arrayListe: ArrayList<String> = db.getAllRecord()
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayListe)
+        ls.adapter = myAdapter
+        chNom.text = ""
+        chID.text.clear()
+    }
+
+    fun Supprimer(view: View) {
+        val chId: TextView = findViewById(R.id.chID)
+        val ls: ListView = findViewById(R.id.maliste)
+        val db = DBconnexion(this)
+        db.deleteRow(chId.text.toString().toInt())
+        val arrayL: ArrayList<String> = db.getAllRecord()
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayL)
+        ls.adapter = myAdapter
+        chId.text = ""
     }
 }
